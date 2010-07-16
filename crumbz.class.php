100bd1c0dba34b2ec2be9f92a43df1dfa32e780d
wshawn <shawn@shawnwilkerson.com>
2010-07-16 11:12:09 -0400

    new file:   crumbz.class.php 	new file:   crumbz.snippet.php 	crumbz/

---
+++ crumbz.class.php
@@ -0,0 +1,178 @@
+<?php
+
+/**
+ *  File		crumbz.class.php (requires MODx Revolution 2.x)
+ * Created on	Jul 02, 2010
+ * Project		shawn_wilkerson
+ * @package 	crumbz
+ * @version	1.0
+ * @category	navigation
+ * @author		W. Shawn Wilkerson
+ * @link		http://www.shawnWilkerson.com
+ * @copyright  Copyright (c) 2010, W. Shawn Wilkerson.  All rights reserved.
+ * @license  	GPL
+ *
+ *
+ * This is the main file to access the breadcrumb class.
+ *
+ */
+
+class Crumbz
+{
+	function __construct(modX & $modx, array $config= array ())
+	{
+		/*
+		 * Import $modx reference
+		 */
+		$this->modx= & $modx;
+		/*
+		 * Establish base settings -- merging incoming overrides
+		 */
+		$this->_config= array_merge(array (
+			'id' => $this->modx->resource->get('id'),
+			'depth' => 10,
+			'format' => 'bar',
+			'seperator' => ' &raquo; ',
+			'textOnly' => false,
+			'removeSiteHome' => false,
+			'removeLastID' => false,
+			'lastChildAsLink' => false,
+			'useLongTitle' => false,
+
+			
+		), $config);
+		/*
+		 * establish parents of user provided document
+		 */
+		$this->parents= $this->modx->getParentIds($this->_config['id'], $this->_config['depth']);
+		/*
+		 * Reverse array to top-down order
+		 */
+		$this->parents= array_reverse($this->parents);
+	}
+
+	/**
+	 * clear resources
+	 */
+	function __destruct()
+	{
+
+		unset ($this->modx, $this->_config, $this->parents, $this->setHome, $this->lastCrumb, $this->getNavigation, $this->links, $this->setFormat);
+	}
+
+	/**
+	 * Processes the request
+	 * @return html formated string of navigation links
+	 */
+	public function run()
+	{
+		$this->setHome();
+		$this->parents[]= $this->setLastCrumb();
+		$this->links= $this->getNavigation();
+		return $this->setFormat();
+	}
+
+	/**
+	 * Gets all the resource id's in the current path or of docID provided
+	 * @return navigation links for current page
+	 */
+	public function getNavigation()
+	{
+		$useText= ($this->_config['useLongTitle'] == true) ? 'longtitle' : 'pagetitle';
+		foreach ($this->parents as $resourceID)
+		{
+			$obj= $this->modx->getObject('modDocument', array (
+				'id' => $resourceID
+			));
+			/*
+			 * Stop processing on deleted documents as the tree will also be deleted
+			 */
+			if ($obj->get('deleted') == false)
+			{
+				/*
+				 * Show only published documents and those not hidden from menus
+				 */
+				if (($obj->get('published') == true) && ($obj->get('hidemenu') == false))
+				{
+					/*
+					 * Capture text to be used for link / text
+					 */
+					$linkText= $obj->get($useText);
+					/*
+					 * Match the title text to the repective linkText.
+					 * pageTitle gets longTitle as Title="" text
+					 * longTitle gets description as Title="" text
+					 */
+					$linkTitle= ($this->_config['useLongTitle'] == true) ? $obj->get('description') : $obj->get('longtitle');
+					$o[]= ($this->_config['textOnly'] == true) ? $linkText : '<a href="'.$this->modx->makeUrl($resourceID).'" title="'.$linkTitle.'">'.$linkText.'</a>';
+				}
+			}
+		}
+		/*
+		 * Replace the last link with straight text
+		 */
+		if ($this->_config['lastChildAsLink'] == true)
+		{
+			array_pop($o);
+			$o[]= $linkText;
+		}
+		/*
+		 * clear resources
+		 */
+		unset ($useText, $resourceID, $obj, $linkText, $linkTitle);
+		return $o;
+	}
+
+	/**
+	 * Sets the format of the breadcrumbs to be returned.
+	 * @return string of html content.
+	 */
+	public function setFormat()
+	{
+		$c= 0;
+		$numLinks= count($this->links);
+		foreach ($this->links as $location)
+		{
+			switch ($this->_config['format'])
+			{
+				case 'list' :
+					$o .= '<li>'.$location.'</li>';
+					break;
+
+				case 'vertical' :
+					$o .= $location.'<br />';
+					break;
+
+				case 'bar' :
+				default :
+					$o .= $location;
+					$o .= ($numLinks === ++ $c) ? '' : $this->_config['seperator'];
+					break;
+			}
+		}
+		return $o;
+	}
+
+	/**
+	 * Based on the removeSiteHome parameter: removes the array key, sets the key to the site_start, or leaves it as set
+	 */
+	public function setHome()
+	{
+		if ($this->_config['removeSiteHome'] == true)
+		{
+			array_shift($this->parents);
+		} else
+		{
+			$this->parents[0]= ($this->parents[0] == 0) ? $this->modx->getOption('site_start') : $this->parents[0];
+		}
+	}
+
+	/**
+	* Based on the removeLastChild parameter: adds the array key, or returns nothing
+	*/
+	public function setLastCrumb()
+	{
+		return ($this->_config['removeLastID'] == false) ? $this->_config['id'] : '';
+	}
+}
+?>
---
+++ crumbz.snippet.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ *  File		crumbz.snippet.php (requires MODx Revolution 2.x)
+ * Created on	Jul 02, 2010
+ * Project		shawn_wilkerson
+ * @package 	crumbz
+ * @version	1.0
+ * @category	navigation
+ * @author		W. Shawn Wilkerson
+ * @link		http://www.shawnWilkerson.com
+ * @copyright  Copyright (c) 2010, W. Shawn Wilkerson.  All rights reserved.
+ * @license  	GPL
+ * 
+ * 
+ * This snippet and associated class perform only the duties of a breadcrumb snippet.
+ * 
+ * It ignores, deleted documents, hidden documents, and unpublished documents, as breadcrumbs should typically
+ * be only those files actually visitable by a web browser. This class also does not concern itself with ACLs.
+ */
+ 
+/* Parameters:
+  	id 				int Resource ID to build Breadcrumbs from (upwards)					'id' => $this->modx->resource->get('id')
+	
+	depth			int Number of resources to return -- Home will always be 		 	'depth' => 10
+					one additional
+	
+	format			string	[list, vertical, bar]  Returns xhtml formated text			'format' => 'bar'
+					needing wrapped inside of ul, table, div.  This allows simple 
+					CSS application via the parent elements
+	
+	seperator		string which divides the bar elements								'seperator' => ' &raquo; '
+	
+	textonly		bool [1/0]	returns only the text from links, not actual links		'textOnly' => false
+	
+	removeSiteHome	bool [1/0] 	removes the 'site_start' / Home document				'removeSiteHome' => false
+	
+	removeLastID	bool [1/0] 	removes the current document or specified document		'removeLastID' => false
+					via &id=`#` above from the result
+	
+	lastChildAsLink bool [1/0] 	optionally returns the last resource (possibly the 		'lastChildAsLink' => false
+					current) document as a link
+					
+	useLongTitle	bool [0/1] 	optionally uses the resource long title as the link 	'useLongTitle' => false
+					text with the resource description returning as the title=""
+					attribute in the href tag. Developer is responsible to ensure
+					those fields are completed in the manager
+			
+*/
+$crumbz_base= $modx->getOption('core_path').'components/crumbz/';
+include_once $crumbz_base.'crumbz.class.php';
+if (!$modx->loadClass('Crumbz', $crumbz_base, true, true))
+{
+	return 'error: Crumbz class not found';
+}
+$crumbz= new Crumbz($modx, $scriptProperties);
+return $crumbz->run();
+?>
